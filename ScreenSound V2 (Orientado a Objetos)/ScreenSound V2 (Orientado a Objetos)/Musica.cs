7d749e308b60d3a794b20f124659e4ec909e6cb5
TomazMPP <tomazmppontes@gmail.com>
2024-02-23 23:52:18 -0300

    :+1: ScreenSound V2

---
+++ ScreenSound V2 (Orientado a Objetos)/ScreenSound V2 (Orientado a Objetos)/Musica.cs
@@ -0,0 +1,19 @@
+﻿class Musica
+{
+    public string nomeDaMusica;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome da Música: {nomeDaMusica}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel) { Console.WriteLine("Música disponível no Plano."); }
+        else
+        {
+            Console.WriteLine("Adquira o Plano Super+");
+        }
+    }
+}
