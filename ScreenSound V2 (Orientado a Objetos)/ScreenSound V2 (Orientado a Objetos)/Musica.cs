68e919205be48edee49a4ef00eccb2605f6d2d97
TomazMPP <tomazmppontes@gmail.com>
2024-02-24 23:25:00 -0300

    :up:

--- ScreenSound V2 (Orientado a Objetos)/ScreenSound V2 (Orientado a Objetos)/Musica.cs
+++ ScreenSound V2 (Orientado a Objetos)/ScreenSound V2 (Orientado a Objetos)/Musica.cs
@@ -1,16 +1,18 @@
 ﻿class Musica
 {
-    public string nomeDaMusica;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string NomeDaMusica { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel {  get; set; }
+
+    public string DescricaoResumida => $"A música {NomeDaMusica} pertence a banda {Artista}";
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome da Música: {nomeDaMusica}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
-        if (disponivel) { Console.WriteLine("Música disponível no Plano."); }
+        Console.WriteLine($"Nome da Música: {NomeDaMusica}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel) { Console.WriteLine("Música disponível no Plano."); }
         else
         {
             Console.WriteLine("Adquira o Plano Super+");
--- ScreenSound V2 (Orientado a Objetos)/ScreenSound V2 (Orientado a Objetos)/Program.cs
+++ ScreenSound V2 (Orientado a Objetos)/ScreenSound V2 (Orientado a Objetos)/Program.cs
@@ -1,14 +1,17 @@
 ﻿Musica musica1 = new Musica();
-musica1.nomeDaMusica = "Roxane";
-musica1.artista = "The Police";
-musica1.duracao = 273;
-musica1.disponivel = true;
+musica1.NomeDaMusica = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
 
 Musica musica2 = new Musica();
-musica2.nomeDaMusica = "Vertigo";
-musica2.artista = "U2";
-musica2.duracao = 367;
-musica2.disponivel = false;
+musica2.NomeDaMusica = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+musica2.Disponivel = false;
 
 musica1.ExibirFichaTecnica();
+Console.WriteLine(musica1.DescricaoResumida);
+Console.WriteLine();
 musica2.ExibirFichaTecnica();
+Console.WriteLine(musica2.DescricaoResumida);
